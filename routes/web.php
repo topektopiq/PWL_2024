2247e3502b7f012d33d9306ac4d184badfb32d5b
topektopiq <hidayatullohtaufiq0@gmail.com>
2024-02-22 14:40:05 +0700

    jobsheet 2 - route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,39 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/nim', function () {
+    return "NIM: 2241720207 NAMA: Ahmad Taufiq Hidayatulloh";
+});
+
+Route::get(	'/user/{name}', function ($name) { 
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+    return 'Pos ke-'.$postId."Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function($articleId) {
+    return "Halaman Artikel dengan ID ".$articleId;
+});
+
+
+Route::get(	'/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name;
+});
+
+// Route::get('/hello', [WelcomeController::class,'hello']);
+
